8d3de02ad320fb150ff96f6f56b0c9d824e5c9d6
Kwangsun Noh <nohkwangsun@gmail.com>
2022-11-19 02:59:08 +0800

    update references

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,14 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println(ProxyBuilder.builder(User.class));
         System.out.println(ProxyBuilder.builder(Book.class));
+
+        /**
+         * refs.
+         * https://medium.com/@joachim.beckers/debugging-an-annotation-processor-using-intellij-idea-in-2018-cde72758b78a
+         * https://hannesdorfmann.com/annotation-processing/annotationprocessing101/
+         * https://www.javacodegeeks.com/2015/09/java-annotation-processors.html
+         * https://www.baeldung.com/java-annotation-processing-builder
+         * https://www.youtube.com/playlist?list=PLQH14oBwDThItQZa8S9r_Tcc3cADt7Fq_
+         */
     }
 }
